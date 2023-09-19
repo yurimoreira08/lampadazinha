c6b77547f28c22a6db644ca6643eec8b2aca7bf4
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2023-09-19 17:26:37 -0500

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,30 @@
+const lamp1 = document.getElementById('lamp1');
+const lamp2 = document.getElementById('lamp2');
+
+
+let lamp1Ligada = false;
+lamp1.addEventListener('click', () => {
+  if (lamp1Ligada) {
+    lamp1.src = 'lampada.jpg';
+    lamp1Ligada = false;
+  } else {
+    lamp1.src = 'lampada-on.jpg';
+    lamp1Ligada = true;
+  }
+});
+
+// Ligar/desligar lâmpada 2 quando o mouse passar sobre ela
+let lamp2Ligada = false;
+lamp2.addEventListener('mouse', () => {
+  if (!lamp2Ligada) {
+    lamp2.src = 'lampada-on.jpg';
+    lamp2Ligada = true;
+  }
+});
+
+lamp2.addEventListener('mouseout', () => {
+  if (lamp2Ligada) {
+    lamp2.src = 'lampada.jpg';
+    lamp2Ligada = false;
+  }
+});
